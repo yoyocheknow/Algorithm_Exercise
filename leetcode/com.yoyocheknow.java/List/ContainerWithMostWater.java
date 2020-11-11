d1919b8e2dac42f6b9cd6951e0d6aabe031e7084
zhihua <911dangerous@gmail.com>
2020-11-11 22:02:45 +0800

    容器接雨水问题

---
+++ leetcode/com.yoyocheknow.java/List/ContainerWithMostWater.java
@@ -0,0 +1,28 @@
+package List;
+
+/**
+ * 容器接雨水问题
+ *
+ * @author zhihua on 2020/11/11
+ */
+public class ContainerWithMostWater {
+    public static int maxArea(int[] height) {
+        int left=0;
+        int right = height.length-1;
+        int maxRain=0;
+        while(left<right){
+            int rain =(right-left)*(Math.min(height[left],height[right]));
+            maxRain = Math.max(maxRain,rain);
+            if(height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return maxRain;
+    }
+    public static void main(String[] agrs){
+        int [] height = new int[]{4,3,2,1,4};
+        System.out.println(maxArea(height));
+    }
+}
