8c13dbdc2c3dc1ed1985ea2aad3668847d162e90
zhihua <911dangerous@gmail.com>
2020-12-30 11:03:31 +0800

    旋转数组的二分查找

--- com/yoyocheknow/java/leetcode/BinarySearch/Search_in_Rotated_Sorted_Array.java
+++ com/yoyocheknow/java/leetcode/BinarySearch/Search_in_Rotated_Sorted_Array.java
@@ -6,8 +6,41 @@ package leetcode.BinarySearch;
  * @author zhihua on 2020/12/29
  */
 public class Search_in_Rotated_Sorted_Array {
-    //此方法是o(n)的时间复杂度。不建议使用
+
     public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while (left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+
+            //左半边有序
+            if(nums[mid]>=nums[left] ){
+                //left<=target<mid 时，在左半边二分查找，否则去右半边
+                if(nums[left]<=target && target<nums[mid]){
+                    right=mid-1;
+                }else{
+                    left=mid+1;
+                }
+            }
+            //右半边有序
+            else{
+                //mid<=target<right 时，在右半边二分查找，否则去左半边
+                if(nums[mid]<=target && target<nums[right]){
+                    left=mid+1;
+                }else{
+                    right=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    //此方法是o(n)的时间复杂度。不建议使用
+    public int search1(int[] nums, int target) {
         //先找到旋转点的idnex值
         int index = findPivotIndex(nums);
         //以index值为中心，分成两半，各自做二分查找
@@ -48,37 +81,6 @@ public class Search_in_Rotated_Sorted_Array {
         return 0;
     }
 
-
-    public int search1(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length-1;
-        while (left<=right){
-            int mid = (left+right)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            if(nums[mid]>target){
-                //target<nums[mid]则应该去左半边
-                //如果mid大于左边说明左半边有序
-                if(nums[mid]>nums[left]){
-                    right = mid-1;
-
-                }
-                //否则的话去右半边
-                else{
-                    left = mid+1;
-                }
-            }
-            if(nums[mid]<target){
-                if(nums[mid+1]<nums[right]){
-                    left = mid+1;
-                }
-                else{
-
-                }
-            }
-        }
-    }
 
     public static void main(String[] args){
         System.out.print(new Search_in_Rotated_Sorted_Array().search(new int[]{4,5,6,7,0,1,2},0));
