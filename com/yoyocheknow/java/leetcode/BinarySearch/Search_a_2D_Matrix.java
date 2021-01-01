e128f866da76ac9ed42bc8543197211eeec2e37a
zhihua <911dangerous@gmail.com>
2021-01-01 10:52:14 +0800

    二维矩阵的二分查找

--- com/yoyocheknow/java/leetcode/BinarySearch/Search_a_2D_Matrix.java
+++ com/yoyocheknow/java/leetcode/BinarySearch/Search_a_2D_Matrix.java
@@ -6,6 +6,10 @@ package leetcode.BinarySearch;
  * @author zhihua on 2020/12/31
  */
 public class Search_a_2D_Matrix {
+    //先在列上按照二分查找思路查找
+    //如果第一行的最后一个数据还是小于target，说明应该去下一行
+    //然后在下一行继续二分查找
+    //时间复杂度为N log(n)
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix.length<1){
             return false;
@@ -14,37 +18,31 @@ public class Search_a_2D_Matrix {
         int right = matrix[0].length-1;
         int top = 0;
         int bottom = matrix.length-1;
-
-        while (left<=right && top<=bottom){
-
+        int row = top;
+        while (left<=right && row<=bottom){
             int colMid = (left+right)/2;
-            while(top<=bottom){
 
-                int rowMid = (top+bottom)/2;
-                if(matrix[rowMid][colMid]==target){
-                    return true;
-                }
-                else if(matrix[rowMid][colMid]<target){
-                    top = rowMid+1;
-                }else{
-                    bottom = rowMid-1;
-                }
-            }
-            if(matrix[top][colMid]==target){
+            if(matrix[row][colMid]==target){
                 return true;
             }
-            else if(matrix[top][colMid]<target){
-                top ++;
-            }else{
-                bottom --;
-            }
+            else if(matrix[row][colMid]>target ){
+                right = colMid-1;
+            }else if(matrix[row][colMid]<target && matrix[row][right]<target){
+                if(row<=bottom){
+                    row = ++top;
+                }else{
+                    left = colMid+1;
+                }
 
+            }else if(matrix[row][colMid]<target && matrix[row][right]>=target){
+                left = colMid+1;
+            }
         }
         return false;
     }
 
     public static void main(String[] args){
-        int[][] matrix = new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-        System.out.print(new Search_a_2D_Matrix().searchMatrix(matrix,60));
+        int[][] matrix = new int[][]{{1},{3}};
+        System.out.print(new Search_a_2D_Matrix().searchMatrix(matrix,3));
     }
 }
