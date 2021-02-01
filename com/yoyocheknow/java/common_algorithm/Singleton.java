966d3b9d596014bc27ace3a84220ec4a9f3b9e46
zhihua <911dangerous@gmail.com>
2021-02-01 18:30:27 +0800

    单例模式

---
+++ com/yoyocheknow/java/common_algorithm/Singleton.java
@@ -0,0 +1,21 @@
+package common_algorithm;
+
+/**
+ * 单例模式
+ *
+ * @author zhihua on 2021/2/1
+ */
+public class Singleton {
+    private volatile static Singleton singleton;
+    private Singleton (){}
+    public static Singleton getSingleton() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
