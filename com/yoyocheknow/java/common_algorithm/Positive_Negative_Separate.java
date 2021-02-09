a73675f0c22a906cd72382023a2554174c395c45
zhihua <911dangerous@gmail.com>
2021-02-09 17:00:09 +0800

    正负数分离

--- com/yoyocheknow/java/common_algorithm/Positive_Negative_Separate.java
+++ com/yoyocheknow/java/common_algorithm/Positive_Negative_Separate.java
@@ -2,7 +2,8 @@ package common_algorithm;
 
 /**
  * 正负数分离
- *
+ * 先用双指针法把 负数和非负数分离
+ * 再遍历一遍，将非负数中的0 和正数分离
  * @author zhihua on 2021/2/9
  */
 public class Positive_Negative_Separate {
@@ -17,6 +18,24 @@ public class Positive_Negative_Separate {
                 left++;
             }
             while(left<=right && array[right]>=0){
+                right--;
+            }
+            if(left<right){
+                int temp = array[left];
+                array[left]=array[right];
+                array[right]=temp;
+            }
+
+        }
+
+
+        left=0;
+        right=array.length-1;
+        while(left<right){
+            while(left<=right && array[left]<=0){
+                left++;
+            }
+            while(left<=right && array[right]>0){
                 right--;
             }
             if(left<right){
